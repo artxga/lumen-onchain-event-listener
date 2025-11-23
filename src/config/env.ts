2f9454a6f15935562a99ad11f7f376020002177d
artxga <angelarteaga989@gmail.com>
2025-11-23 11:28:47 -0500

    envs

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,16 +2,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-  tronNode: process.env.TRON_FULL_NODE!, // TODO: Remove
-
-  polygonNode: process.env.POLYGON_FULL_NODE!, // TODO: Remove
+  // tronNode: process.env.TRON_FULL_NODE!, // TODO: Remove
+  // polygonNode: process.env.POLYGON_FULL_NODE!, // TODO: Remove
+  // pollLevels: (process.env.POLL_LEVELS ?? "10000,30000,60000,120000")
+  //   .split(",")
+  //   .map(Number),
+  // maxIdlePolls: Number(process.env.MAX_IDLE_POLLS ?? 3),
+  // apiUrl: process.env.INTERNAL_API_URL!,
 
   polygonWs: process.env.POLYGON_WS!,
   contractAddress: process.env.PAYMENT_EXECUTOR!,
-  apiUrl: process.env.INTERNAL_API_URL!,
-  pollLevels: (process.env.POLL_LEVELS ?? "10000,30000,60000,120000")
-    .split(",")
-    .map(Number),
-  maxIdlePolls: Number(process.env.MAX_IDLE_POLLS ?? 3),
   rabbitUrl: process.env.RABBIT_URL!,
 };
